6b0c8f823d5fb50d5826402f2ae47a2dd8b174d3
Ronaldo Potenciano <rbpotenciano@yahoo.com>
2022-03-10 20:07:18 +0000

    Update files

---
+++ Basic JavaScript Projects/Assignments/Assignment12.js
@@ -0,0 +1,8 @@
+var Family = "The Santos", Dad = "Pedro", Mom = "Nene",
+    Daughter = "Irene", Son = "Miguel";
+var Family = Family.fontcolor("red"),  Dad = Dad.fontcolor("blue"), Mom = Mom.fontcolor("green"), 
+Daughter = Daughter.fontcolor("brown"), Son = Son.fontcolor("orange");
+    
+document.write(Son);
+
+window.alert("How are you today?");
--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,13 @@
-window.alert("Hello, World!"); 
+
+
+var Family = "The Santos", Dad = "Pedro", Mom = "Nene",             //Definining multiple variables and giving
+    Daughter = "Irene", Son = "Miguel";                             //them string values
+var Family = Family.fontcolor("red"),  Dad = Dad.fontcolor("blue"), //Using the fontcolor method on string variable
+Mom = Mom.fontcolor("green"), Daughter = Daughter.fontcolor("brown"), 
+Son = Son.fontcolor("orange");
+    
+document.write(Son);                                                //Displaying text using document.write method 
+
+window.alert("How are you today?");                                 //Using an alert window to pop up a message
+
+document.write(3+3); //This is statement containing an expression (3 + 3).
